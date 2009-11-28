c7a927dcdb3d554cfe7f1198582fc34cc4539094
Vrai Stacey <vrai.stacey@gmail.com>
2009-11-28 17:42:26 +0000

    Split codec.c in to encode and decode specific files. Added license header to Makefile rules.

---
+++ src/codec_decode.c
@@ -0,0 +1,173 @@
+/**
+ * Copyright (C) 2009 - 2009, Vrai Stacey.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "fudge/codec.h"
+#include "fudge/platform.h"
+#include "header.h"
+#include <assert.h>
+
+FudgeStatus FudgeCodec_decodeMsgFields ( FudgeMsg message, const fudge_byte * bytes, fudge_i32 numbytes );
+
+
+#define FUDGECODEC_TYPEDARRAYFIELD_DECODE_IMPL( type, typename, swapper )                                                                       \
+    FudgeStatus FudgeCodec_decode##typename##Field ( FudgeMsg message, FudgeFieldHeader header, const type * elements, fudge_i32 numelements )  \
+    {                                                                                                                                           \
+        int index;                                                                                                                              \
+        type * mutable;                                                                                                                         \
+        FudgeStatus status;                                                                                                                     \
+        if ( ! ( mutable = ( type * ) malloc ( numelements * sizeof ( type ) ) ) )                                                              \
+            return FUDGE_OUT_OF_MEMORY;                                                                                                         \
+        for ( index = 0; index < numelements; ++index )                                                                                         \
+            mutable [ index ] = swapper ( elements [ index ] );                                                                                 \
+        status = FudgeMsg_addField##typename##Array ( message, header.name, FudgeHeader_getOrdinal ( &header ), mutable, numelements );         \
+        free ( mutable );                                                                                                                       \
+        return status;                                                                                                                          \
+    }
+
+FUDGECODEC_TYPEDARRAYFIELD_DECODE_IMPL( fudge_i16, I16, ntohs )
+FUDGECODEC_TYPEDARRAYFIELD_DECODE_IMPL( fudge_i32, I32, ntohl )
+FUDGECODEC_TYPEDARRAYFIELD_DECODE_IMPL( fudge_i64, I64, ntohi64 )
+FUDGECODEC_TYPEDARRAYFIELD_DECODE_IMPL( fudge_f32, F32, ntohf )
+FUDGECODEC_TYPEDARRAYFIELD_DECODE_IMPL( fudge_f64, F64, ntohd )
+
+FudgeStatus FudgeCodec_decodeField ( FudgeMsg message, FudgeFieldHeader header, fudge_i32 width, const fudge_byte * bytes, fudge_i32 numbytes )
+{
+    FudgeStatus status;
+
+    if ( width > numbytes )
+        return FUDGE_OUT_OF_BYTES;
+
+    switch ( header.type )
+    {
+        /* Fixed width types are handled individually */
+        case FUDGE_TYPE_INDICATOR:  return FudgeMsg_addFieldIndicator ( message, header.name, FudgeHeader_getOrdinal ( &header ) );
+        case FUDGE_TYPE_BOOLEAN:    return FudgeMsg_addFieldBool ( message, header.name, FudgeHeader_getOrdinal ( &header ), *bytes != 0 );
+        case FUDGE_TYPE_BYTE:       return FudgeMsg_addFieldByte ( message, header.name, FudgeHeader_getOrdinal ( &header ), *( ( fudge_byte * ) bytes ) );
+        case FUDGE_TYPE_SHORT:      return FudgeMsg_addFieldI16  ( message, header.name, FudgeHeader_getOrdinal ( &header ), ntohs ( *( ( fudge_i16 * ) bytes ) ) );
+        case FUDGE_TYPE_INT:        return FudgeMsg_addFieldI32  ( message, header.name, FudgeHeader_getOrdinal ( &header ), ntohl ( *( ( fudge_i32 * ) bytes ) ) );
+        case FUDGE_TYPE_LONG:       return FudgeMsg_addFieldI64  ( message, header.name, FudgeHeader_getOrdinal ( &header ), ntohi64 ( *( ( fudge_i64 * ) bytes ) ) );
+        case FUDGE_TYPE_FLOAT:      return FudgeMsg_addFieldF32  ( message, header.name, FudgeHeader_getOrdinal ( &header ), ntohf ( *( ( fudge_f32 * ) bytes ) ) );
+        case FUDGE_TYPE_DOUBLE:     return FudgeMsg_addFieldF64  ( message, header.name, FudgeHeader_getOrdinal ( &header ), ntohd ( *( ( fudge_f64 * ) bytes ) ) );
+
+        /* Submessages should be a simple list of fields */
+        case FUDGE_TYPE_FUDGE_MSG:
+        {
+            FudgeMsg submessage;
+
+            if ( ( status = FudgeMsg_create ( &submessage ) ) != FUDGE_OK )
+                return status;
+            if ( ( status = FudgeCodec_decodeMsgFields ( submessage, bytes, width ) ) == FUDGE_OK )
+                status = FudgeMsg_addFieldMsg ( message, header.name, FudgeHeader_getOrdinal ( &header ), submessage );
+            FudgeMsg_release ( submessage );
+            return status;
+        }
+
+        /* Typed arrays need to be converted to host ordering */
+        case FUDGE_TYPE_SHORT_ARRAY:
+            return FudgeCodec_decodeI16Field ( message, header, ( const fudge_i16 * ) bytes, width / sizeof ( fudge_i16 ) );
+        case FUDGE_TYPE_INT_ARRAY:
+            return FudgeCodec_decodeI32Field ( message, header, ( const fudge_i32 * ) bytes, width / sizeof ( fudge_i32 ) );
+        case FUDGE_TYPE_LONG_ARRAY:
+            return FudgeCodec_decodeI64Field ( message, header, ( const fudge_i64 * ) bytes, width / sizeof ( fudge_i64 ) );
+        case FUDGE_TYPE_FLOAT_ARRAY:
+            return FudgeCodec_decodeF32Field ( message, header, ( const fudge_f32 * ) bytes, width / sizeof ( fudge_f32 ) );
+        case FUDGE_TYPE_DOUBLE_ARRAY:
+            return FudgeCodec_decodeF64Field ( message, header, ( const fudge_f64 * ) bytes, width / sizeof ( fudge_f64 ) );
+
+        /* Everything else can be loaded as a block of bytes */
+        default:
+            return FudgeMsg_addFieldOpaque ( message, header.type, header.name, FudgeHeader_getOrdinal ( &header ), bytes, width );
+    }
+}
+
+
+FudgeStatus FudgeCodec_decodeMsgFields ( FudgeMsg message, const fudge_byte * bytes, fudge_i32 numbytes )
+{
+    FudgeStatus status;
+    FudgeFieldHeader fieldheader;
+
+    while ( numbytes )
+    {
+        fudge_i32 consumed, width;
+
+        if ( ( status = FudgeHeader_decodeFieldHeader ( &fieldheader, &consumed, bytes, numbytes ) ) != FUDGE_OK )
+            return status;
+        bytes += consumed;
+        numbytes -= consumed;
+
+        /* Get the field width */
+        if ( ( status = FudgeHeader_getFieldWidth ( &width, &consumed, fieldheader, bytes, numbytes ) ) != FUDGE_OK )
+            goto release_fieldheader_and_fail;
+        bytes += consumed;
+        numbytes -= consumed;
+
+        /* Get the field and add it to the message */
+        if ( ( status = FudgeCodec_decodeField ( message, fieldheader, width, bytes, numbytes ) ) != FUDGE_OK )
+            goto release_fieldheader_and_fail;
+        bytes += width;
+        numbytes -= width;
+
+        /* Clean up this iteration */
+        FudgeHeader_destroyFieldHeader ( fieldheader );
+    }
+    
+    return FUDGE_OK;
+
+/* Sorry Dijkstra */
+release_fieldheader_and_fail:
+    FudgeHeader_destroyFieldHeader ( fieldheader );
+    return status;
+}
+
+/******************************************************************************
+ * Externally accessible functions
+ */
+
+FudgeStatus FudgeCodec_decodeMsg ( FudgeMsgEnvelope * envelope, const fudge_byte * bytes, fudge_i32 numbytes )
+{
+    FudgeStatus status;
+    FudgeMsgHeader header;
+    FudgeMsg message;
+
+    if ( ! envelope )
+        return FUDGE_NULL_POINTER;
+
+    /* Get the message header and use it to create the envelope and the message */
+    if ( ( status = FudgeHeader_decodeMsgHeader ( &header, bytes, numbytes ) ) != FUDGE_OK )
+        return status;
+    if ( numbytes < header.numbytes )
+        return FUDGE_OUT_OF_BYTES;
+    if ( ( status = FudgeMsg_create ( &message ) ) != FUDGE_OK )
+        return status;
+
+    envelope->directives = header.directives;
+    envelope->schemaversion = header.schemaversion;
+    envelope->taxonomy = header.taxonomy;
+    envelope->message = message;
+
+    /* Advance to the end of the header */
+    bytes += sizeof ( FudgeMsgHeader );
+    numbytes -= sizeof ( FudgeMsgHeader );
+
+    /* Consume fields */
+    if ( ( status = FudgeCodec_decodeMsgFields ( message, bytes, numbytes ) ) != FUDGE_OK )
+    {
+        FudgeMsg_release ( message );
+        envelope->message = 0;
+    }
+
+    return status;
+}
+
